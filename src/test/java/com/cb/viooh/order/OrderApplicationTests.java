6fb1cc372f311d630dd9a9b3624fed05884c4a2e
jmandal <jitendra.92mandal@gmail.com>
2021-06-06 19:47:20 +0530

    commented tc

--- src/test/java/com/cb/viooh/order/OrderApplicationTests.java
+++ src/test/java/com/cb/viooh/order/OrderApplicationTests.java
@@ -3,11 +3,11 @@ package com.cb.viooh.order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OrderApplicationTests {
 
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
