8b7186c8eaa82694f109f81158e85c81c55a9127
jmandal <jitendra.92mandal@gmail.com>
2021-09-06 16:34:44 +0530

    added swagger and actuator

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:8-jdk-alpine
-MAINTAINER njrtriplicity@gmail.com
-
-ADD target/*.jar app.jar
-
-
-EXPOSE 52026
-
-ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/cb/viooh/order/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.cb.viooh.order.config;
+
+import java.util.Date;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).directModelSubstitute(XMLGregorianCalendar.class, Date.class)
+				.groupName("njrtriplicity").select()
+				.apis(RequestHandlerSelectors.basePackage("com.cb.viooh.order")).paths(PathSelectors.any())
+				.build().apiInfo(metaData());
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("NJR Order")
+				.description("\"Order Component \"")
+				.contact(new Contact("njrtriplicity", "", "njrtriplicity@gmail.com")).version("1.0.0").build();
+	}
+}
